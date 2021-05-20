63c1d7224b937764aa38a522d5b5a254a5df2b23
betiix26 <81751603+betiix26@users.noreply.github.com>
2021-05-20 22:53:34 +0300

    Add files via upload

---
+++ Tanks/Scripts/BigPowerUp.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BigPowerUp : MonoBehaviour
+{
+   public float multiplier = 1.4f;
+    private float waiting = 5f;
+    void OnTriggerEnter (Collider other)
+    {
+        StartCoroutine(Pickup(other));
+    }
+
+    IEnumerator Pickup(Collider player)
+    {
+        
+        player.transform.localScale *=multiplier;
+        ShellExplosion dmg = player.GetComponent<ShellExplosion>();
+
+        GetComponent<MeshRenderer>().enabled = false;
+        GetComponent<Collider>().enabled = false;
+        
+        yield return new WaitForSeconds (waiting);
+
+        player.transform.localScale /=multiplier;
+
+        Destroy(gameObject);
+    }
+}
---
+++ Tanks/Scripts/HealthPowerUp.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPowerUp : MonoBehaviour
+{
+    public float fullHealth=100f;
+
+    void OnTriggerEnter (Collider other)
+    {
+        Pickup(other);
+    }
+
+    void Pickup(Collider player)
+    {
+        
+        TankHealth viata = player.GetComponent<TankHealth>();
+        viata.m_CurrentHealth = fullHealth;
+        viata.SetHealthUI();
+
+        Destroy(gameObject);
+    }
+}
